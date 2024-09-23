1affa4491ab187fa952245b017e04e8dced5384e
tonton96 <lcv0611@gmail.com>
2024-09-23 23:44:53 +0700

    update test3_p165

---
+++ exam/ebook.h
@@ -0,0 +1,105 @@
+#include "iostream"
+#include "vector"
+#include "algorithm"
+#include "cmath"
+
+namespace test3_p165
+{
+    const int MAX_SIXE = 128;
+
+    int table[MAX_SIXE][MAX_SIXE];
+
+    std::string trace(const std::string &input)
+    {
+        std::string output;
+        output.resize(input.size() + table[0][input.size() - 1]);
+
+        int from = 0, to = output.size() - 1;
+        int i = 0, j = input.size() - 1;
+        while (true)
+        {
+            if (i == j)
+            {
+                output[from] = output[to] = input[i];
+                break;
+            }
+            if (i > j)
+            {
+                break;
+            }
+            if (input[i] == input[j])
+            {
+                output[from] = output[to] = input[i];
+                from++;
+                to--;
+                i++;
+                j--;
+            }
+            else
+            {
+                if (table[i][j] == table[i + 1][j] + 1)
+                {
+                    output[from++] = input[i];
+                    output[to--] = input[i];
+                    i++;
+                }
+                else
+                {
+                    output[from++] = input[j];
+                    output[to--] = input[j];
+                    j--;
+                }
+            }
+        }
+
+        return output;
+    }
+
+    void init(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            table[i][i] = 1;
+        }
+    }
+
+    std::string handler(const std::string &input)
+    {
+        int size = input.size();
+        init(size);
+
+        int N = input.size();
+        for (int j = 1; j < N; j++)
+        {
+            for (int i = 0; i < N - j; i++)
+            {
+                int from = i;
+                int to = j + i;
+                if (from + 1 <= to - 1)
+                {
+                    if (input[from] == input[to])
+                    {
+                        table[from][to] = table[from + 1][to - 1];
+                    }
+                    else
+                    {
+                        table[from][to] = std::min(table[from + 1][to], table[from][to - 1]) + 1;
+                    }
+                }
+                else
+                {
+                    table[i][j] = std::min(table[from + 1][to], table[from][to - 1]) + 1;
+                }
+            }
+        }
+        auto val = table[0][size - 1];
+
+        return trace(input);
+    }
+
+    void test()
+    {
+        std::string S = "abcdefg_hh_gfe_1_d_2_c_3_ba";
+        std::cout << "Output T: " << handler(S) << std::endl;
+    }
+}
